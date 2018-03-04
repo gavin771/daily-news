dfe77ef115e6bd112152b2a31e6a22eb9ea25da9
gavin771 <gavin.samuels10@gmail.com>
2018-03-04 11:19:01 -0800

    Added layout for latest news component

--- src/components/home/LatestNews.js
+++ src/components/home/LatestNews.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LatestNews = (props) => {
+
+  const showLatest = ({ latest }) => {
+    if (latest) {
+      return latest.map((i) => {
+        return (
+          <Link to={`/news/${i.id}`} key={i.id} className="item">
+            <div
+              className="image_cover"
+              style={{ background: `url(/images/articles/${i.img})` }}
+            >
+              <div className="description">
+                <span>{i.category}</span>
+                <div>{i.title}</div>
+              </div>
+            </div>
+          </Link>
+        )
+      })
+    }
+  }
   return (
-    <div>
-      latest news
+    <div className="home_latest">
+      {showLatest(props)}
     </div>
   )
 }
