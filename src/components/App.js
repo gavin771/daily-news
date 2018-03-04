0f43f9880743593ffe7a02009f0f6b70ebec360e
gavin771 <gavin.samuels10@gmail.com>
2018-03-03 22:25:28 -0800

    added header and footer sections

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+//components
+import Header from './Header';
+import Footer from './Footer';
+
+//containers that get data from redux state
+import Home from '../containers/Home';
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        Hi
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
