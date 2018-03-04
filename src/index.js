36c733bdd42b8b5e85a43c55cfa76a377630170b
gavin771 <gavin.samuels10@gmail.com>
2018-03-03 22:04:30 -0800

    Setup folder structure and redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers'
+const creatStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore);
+
+ReactDOM.render(
+  <Provider store={creatStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
